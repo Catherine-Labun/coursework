a75c0aab4d9d6d5f8dac45bb018abf69a0df8d91
Catherine-Labun <labun2002@gmail.com>
2021-05-16 12:10:45 +0200

    first commit

---
+++ dataset.py
@@ -0,0 +1,42 @@
+from torchvision import datasets
+from torch.utils.data import Dataset
+from PIL import Image
+import random
+import os
+import pandas as pd
+import numpy as np
+
+
+class Paintings(Dataset):
+    def __init__(self, is_train=True, transforms=None):
+        self.data = None
+        self.is_train = is_train
+        self.transforms = transforms
+
+        images = pd.read_csv('images.txt', sep=' ',names=['img_id', 'image_path'])
+        class_label = pd.read_csv('images_class_labels.txt', sep=' ', names=['img_id', 'label'])
+
+        self.data = images.merge(class_label, on='img_id')
+        if is_train:
+            self.data = self.data[self.data['label'] < 25]
+        else:
+            self.data = self.data[self.data['label'] >= 25]
+            self.data['label'] -= 25
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        sample = self.data.iloc[idx]
+        path = sample.image_path
+        label = sample.label
+        image = Image.open(path)
+        image = np.array(image)
+
+        if len(image.shape) == 2:
+            image = np.stack((image, )*3, axis=-1)
+
+        if self.transforms is not None:
+            image = self.transforms(image)
+
+        return image, label
